7c0bef9a44103432508691e966c360d0dc54f8e7
billchen247 <157920313+billchen247@users.noreply.github.com>
2026-04-05 17:39:18 -0400

    Add using directive for Microsoft.AspNetCore.Hosting

--- Tests/CustomWebApplicationFactor.cs
+++ Tests/CustomWebApplicationFactor.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.Hosting;
 using System.Text;
 using System.Net;
 using System.Text.Json;
 using System.Net.Http.Headers;
 using MinimalApi.ViewModels;
+
 
 namespace MinimalApi.Tests;
 
